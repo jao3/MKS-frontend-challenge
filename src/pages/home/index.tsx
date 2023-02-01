fce2649615f51ce89ef74f2c00cf7133d133ef96
Joao Araujo <joaoo.bio@gmail.com>
2023-02-01 20:12:07 -0300

    correções

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,8 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import ProductCard from "@/components/ProductCard";
 import { Header } from "@/components/Header";
-import { Content } from "./styles";
+
 import Footer from "@/components/Footer";
+import styled from "styled-components";
+const Content = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 4rem 15%;
+`;
 
 const Home: React.FC = () => {
   return (
--- src/pages/home/styles.ts
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const Content = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    padding: 4rem 15%;
-`;
